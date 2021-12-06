6a5a1dbcdcff0331457adc59a60e230bf07da36a
Stan Smith <stan@diamondfoundry.com>
2021-12-06 10:13:57 -0700

    WIP: Solve Day 5: Part 1

--- app/Solvers/Day05.ts
+++ app/Solvers/Day05.ts
@@ -1,9 +1,54 @@
 export const solvePart1 = async (input: string) => {
-  return input
+  input = `0,9 -> 5,9
+8,0 -> 0,8
+9,4 -> 3,4
+2,2 -> 2,1
+7,0 -> 7,4
+6,4 -> 2,0
+0,9 -> 2,9
+3,4 -> 1,4
+0,0 -> 8,8
+5,5 -> 8,2`
+
+  const inputParsed = input.split('\n').filter((value) => value !== '')
+
+  // NOTE: x is horizontal, y is vertical (x,y -> x,y)
+  // So, the horizontal and vertical lines from the above list would produce the following diagram:
+  // .......1..
+  // ..1....1..
+  // ..1....1..
+  // .......1..
+  // .112111211
+  // ..........
+  // ..........
+  // ..........
+  // ..........
+  // 222111....
+
+  // TODO: determine the number of points where at least two lines overlap
+
+  // Create the dot matrix
+  const numbersSorted = inputParsed
+    .map((lineSegment) => lineSegment.split(' -> '))
+    .flat()
+    .map((coordinate) =>
+      coordinate.split(',').map((value, index) => {
+        if (index === 0) {
+          return { x: parseInt(value) }
+        }
+        return { y: parseInt(value) }
+      })
+    )
+    .flat()
+    .sort()
+
+  // const maxX = numbersSorted.slice(-1).reduce((_parsedValue, value) => parseInt(value), 0)
+
+  return { numbersSorted, inputParsed }
 }
 
 export const solvePart2 = async (input: string) => {
-  return input
+  return input.split('\n').filter((value) => value !== '')
 }
 
 export default {

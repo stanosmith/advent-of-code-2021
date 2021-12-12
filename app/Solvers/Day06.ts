a54b033d5a14f8e8d108e4cd335404f706f46278
Stan Smith <choicedesign@gmail.com>
2021-12-11 23:44:23 -0700

    Solve Day 6: Part 1 ⭐️

--- app/Solvers/Day06.ts
+++ app/Solvers/Day06.ts
@@ -1,9 +1,41 @@
 export const solvePart1 = async (input: string) => {
-  return input
+  // input = '3,4,3,1,2'
+
+  // How many lanternfish would there be after 80 days?
+  let totalDays = 80 // Test values: 18 or 80
+
+  let simulation = parseInput(input)
+
+  for (let i = 0; i < totalDays; i++) {
+    simulation = runLifecycle(simulation)
+  }
+
+  return simulation.length
+}
+function runLifecycle(ages) {
+  for (let i = 0; i < ages.length; i++) {
+    const age = ages[i]
+    if (age === 0) {
+      ages[i] = 6
+      ages.push(9) // Pushing 9 because it gets decremented at the end of the loop
+    } else {
+      ages[i] = age - 1
+    }
+  }
+  return ages
 }
 
 export const solvePart2 = async (input: string) => {
+  return parseInput(input)
+}
+
+function parseInput(input) {
   return input
+    .split('\n')
+    .filter((value) => value !== '')
+    .join('')
+    .split(',')
+    .map((value) => parseInt(value))
 }
 
 export default {

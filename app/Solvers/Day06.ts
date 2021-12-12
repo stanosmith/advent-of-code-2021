2ccff3167b0eccc67fead4b0eb773ed45d947a60
Stan Smith <choicedesign@gmail.com>
2021-12-12 00:21:31 -0700

    WIP: Solve Day 6: Part 2 - Make note about needing to loop through the parts in the DaysController in order to avoid unnecessary JS processing.

--- app/Solvers/Day06.ts
+++ app/Solvers/Day06.ts
@@ -1,23 +1,24 @@
-export const solvePart1 = async (input: string) => {
+export const solvePart1 = async (input: string, totalDays?: number) => {
   // input = '3,4,3,1,2'
 
-  // How many lanternfish would there be after 80 days?
-  let totalDays = 80 // Test values: 18 or 80
+  // Part 1: How many lanternfish would there be after 80 days?
+  if (!totalDays) {
+    totalDays = 80 // Test value: 18
+  }
 
   let simulation = parseInput(input)
-
   for (let i = 0; i < totalDays; i++) {
     simulation = runLifecycle(simulation)
   }
-
   return simulation.length
 }
+
 function runLifecycle(ages) {
   for (let i = 0; i < ages.length; i++) {
     const age = ages[i]
     if (age === 0) {
       ages[i] = 6
-      ages.push(9) // Pushing 9 because it gets decremented at the end of the loop
+      ages.push(9) // HACK: Pushing 9, instead of 8, because it gets decremented at the end of the loop
     } else {
       ages[i] = age - 1
     }
@@ -26,7 +27,18 @@ function runLifecycle(ages) {
 }
 
 export const solvePart2 = async (input: string) => {
+  // Part 2: How many lanternfish would there be after 256 days?
+
+  // TODO: This is the first time I've needed to have a process run in the background
+  //   The best way to solve this is in the DaysController. There is a comment noting the
+  //   need to loop through the parts instead of imperatively processing each part. This
+  //   way the solve will only be requested if there isn't already an answer.
+
   return parseInput(input)
+}
+
+export const day06Part2Process = async (input: string) => {
+  return await solvePart1(input, 256)
 }
 
 function parseInput(input) {

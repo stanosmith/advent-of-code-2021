48643e157e00b35376acb7b5c430717501850e42
Stan Smith <stan@diamondfoundry.com>
2021-12-17 15:46:57 -0700

    Stub out Day 7: Part 1

---
+++ app/Solvers/Day07.ts
@@ -0,0 +1,24 @@
+export const solvePart1 = async (input: string) => {
+  input = '16,1,2,0,4,2,7,1,2,14\n'
+
+  // `input` represents the horizontal position of each crab
+
+  return parseInput(input)
+}
+
+export const solvePart2 = async (input: string) => {
+  // input =
+  return input
+}
+
+function parseInput(input: string): Array<number> {
+  return input
+    .replace('\n', '')
+    .split(',')
+    .map((value) => parseInt(value))
+}
+
+export default {
+  solvePart1,
+  solvePart2,
+}
--- app/Solvers/index.ts
+++ app/Solvers/index.ts
@@ -4,6 +4,7 @@ import day03 from './Day03'
 import day04 from './Day04'
 import day05 from './Day05'
 import day06 from './Day06'
+import day07 from './Day07'
 
 export default {
   day01,
@@ -12,4 +13,5 @@ export default {
   day04,
   day05,
   day06,
+  day07,
 }

8d12a1653daaf98cfed6317abd0bb4605dc85001
Stan Smith <stan@diamondfoundry.com>
2021-12-17 16:31:10 -0700

    Refactor getting Day data

--- app/Controllers/Http/DaysController.ts
+++ app/Controllers/Http/DaysController.ts
@@ -26,86 +26,120 @@ export default class DaysController {
   public async show({ params, response }: HttpContextContract) {
     // Get cached day, if not found, get from Airtable and then cache it
     const { yearName, name } = params
-    let localPath = Application.tmpPath(`inputs/day-${yearName}-${name.padStart(2, '0')}.json`)
-    let day
+    const day = await getDay(params)
 
-    if (useCache) {
-      try {
-        // Get day data from the local file system
-        const fileBuffer = await Drive.get(localPath)
-        day = JSON.parse(fileBuffer.toString())
-      } catch (e) {
-        console.info('Day data not cached, file will be loaded from Airtable')
-      }
+    // Send 404 if day is not found
+    if (!day || !day.fields) {
+      return response.safeStatus(404).send(`Sorry, no data found for ${name}/${yearName}. 😔`)
     }
 
-    if (!day) {
-      try {
-        // Pull Day data from Airtable
-        const page = await base(tableName)
-          .select({
-            filterByFormula: `AND(name="${name}",yearName="${yearName}")`,
-          })
-          .firstPage()
-
-        // There should only be one item in the array
-        day = page[0]
-      } catch (e) {
-        console.error(e)
-      }
-    }
-
-    if (day) {
-      // Cache the day data
-      await Drive.put(localPath, JSON.stringify(day))
-
-      // TODO: Loop through the parts, load their data and only solve if there is no answer
-
-      // Get input from Airtable
-      if (day.fields.inputs && Array.isArray(day.fields.inputs)) {
-        // inputExample = {
-        //   "id": "attTgDPOonsptzEYC",
-        //   "url": "https://dl.airtable.com/.attachments/719fa6eb92780979dbcf0a7a0c5c04a5/a2cee7a2/input.txt",
-        //   "filename": "input.txt",
-        //   "size": 9311,
-        //   "type": "text/plain"
-        // }
-        const dayName = day.fields.name ?? ('' as String)
-
-        const inputs = day.fields.inputs.map(({ filename, url }) => ({
-          filename,
-          url,
-          isOgInput: filename === 'input.txt',
-        }))
-
-        const defaultInputData = inputs.find((input) => input.isOgInput)
-
-        // No default input found
-        if (!defaultInputData) {
-          throw new Error(`No default input found for Day ${dayName}. 🤷`)
-        }
-
-        // Load the default input text file
-        const input = await got(defaultInputData.url).text()
-
-        // Solve each part
-        const daySolvers = solvers[`day${dayName.toString().padStart(2, '0')}`]
-
-        let part1 = {}
-        let part2 = {}
-
-        // If we have solvers, solve each part
-        if (typeof daySolvers !== 'undefined') {
-          part1 = { answer: await daySolvers.solvePart1(input) }
-          part2 = { answer: await daySolvers.solvePart2(input) }
-        }
-
-        return response.send({ part1, part2, ...day })
-      }
-
+    // If there is no input data, throw an Error
+    if (!day.fields.inputs && !Array.isArray(day.fields.inputs)) {
       throw new Error(`No inputs found for Day ${day.fields.name}. 🤷`)
     }
 
-    return response.safeStatus(404).send(`Sorry, no data found for ${name}/${yearName}. 😔`)
+    // Get input from Airtable
+    // inputExample = {
+    //   "id": "attTgDPOonsptzEYC",
+    //   "url": "https://dl.airtable.com/.attachments/719fa6eb92780979dbcf0a7a0c5c04a5/a2cee7a2/input.txt",
+    //   "filename": "input.txt",
+    //   "size": 9311,
+    //   "type": "text/plain"
+    // }
+    const dayName = day.fields.name ?? ''
+
+    const inputs = day.fields.inputs.map(({ filename, url }) => ({
+      filename,
+      url,
+      isOgInput: filename === 'input.txt',
+    }))
+
+    const defaultInputData = inputs.find((input) => input.isOgInput)
+
+    // No default input found
+    if (!defaultInputData) {
+      throw new Error(`No default input found for Day ${dayName}. 🤷`)
+    }
+
+    // Load the default input text file
+    const input = await got(defaultInputData.url).text()
+
+    // TODO: Loop through the parts, load their data and only solve if there is no answer
+    // Solve each part
+    const daySolvers = solvers[`day${dayName.toString().padStart(2, '0')}`]
+
+    let part1 = {}
+    let part2 = {}
+
+    // If we have solvers, solve each part
+    if (typeof daySolvers !== 'undefined') {
+      part1 = { answer: await daySolvers.solvePart1(input) }
+      part2 = { answer: await daySolvers.solvePart2(input) }
+    }
+
+    return response.send({ part1, part2, ...day })
   }
+}
+
+interface Attachment {
+  id: string
+  url: string
+  filename: string
+  size: number
+  type: string
+}
+interface DayFields {
+  name: string
+  parts: string[]
+  inputs: Attachment[]
+  year: string[]
+  yearName: string[]
+}
+interface Day {
+  _table: Object
+  id: string
+  _rawJson: {
+    id: string
+    fields: DayFields
+    createdTime: string
+  }
+  fields: DayFields
+}
+
+async function getDay({ yearName, name }: Record<string, any>): Promise<Day | void> {
+  let localPath = Application.tmpPath(`inputs/day-${yearName}-${name.padStart(2, '0')}.json`)
+  let day
+
+  // Get day data from the local file system
+  if (useCache) {
+    try {
+      const fileBuffer = await Drive.get(localPath)
+      day = JSON.parse(fileBuffer.toString())
+    } catch (e) {
+      console.info('Day data not cached, file will be loaded from Airtable')
+    }
+  }
+
+  // Pull Day data from Airtable
+  if (!day) {
+    try {
+      const page = await base(tableName)
+        .select({
+          filterByFormula: `AND(name="${name}",yearName="${yearName}")`,
+        })
+        .firstPage()
+
+      // There should only be one item in the array
+      day = page[0]
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  // Cache the day data
+  if (day) {
+    await Drive.put(localPath, JSON.stringify(day))
+  }
+
+  return day
 }

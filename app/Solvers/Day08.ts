3dd320761637e7a91be34d1cf8b83d0e62352ec9
Stan Smith <stan@diamondfoundry.com>
2021-12-19 20:13:41 -0700

    Stub out Day 8: Part 1

---
+++ app/Solvers/Day08.ts
@@ -0,0 +1,15 @@
+export const solvePart1 = async (input: string) => {
+  // input =
+  return parseInput(input)
+}
+
+export const solvePart2 = async (input: string) => {
+  // input =
+  return parseInput(input)
+}
+
+function parseInput(input: string) {
+  return input.split('\n').filter((value) => value !== '')
+}
+
+export default [solvePart1, solvePart2]
--- app/Solvers/index.ts
+++ app/Solvers/index.ts
@@ -5,6 +5,7 @@ import day04 from './Day04'
 import day05 from './Day05'
 import day06 from './Day06'
 import day07 from './Day07'
+import day08 from './Day08'
 
 export default {
   day01,
@@ -14,4 +15,5 @@ export default {
   day05,
   day06,
   day07,
+  day08,
 }

7720b675254475a21d141cd9288a7c4e654e6a42
Stan Smith <choicedesign@gmail.com>
2021-12-23 13:06:26 -0700

    WIP: Day 11: Part 1

--- app/Solvers/Day11.ts
+++ app/Solvers/Day11.ts
@@ -10,9 +10,85 @@ export const solvePart1 = async (input: string) => {
 4846848554
 5283751526
 `
+  const steps = 10
+
+  //   input = `11111
+  // 19991
+  // 19191
+  // 19991
+  // 11111
+  // `
+  // const steps = 2
 
   // How many total flashes are there after 100 steps?
-  return parseInput(input)
+
+  const initialState = parseInput(input).map((row) =>
+    row.split('').map((value) => parseInt(value, 10))
+  )
+
+  let recordedSteps = [initialState]
+
+  for (let i = 0; i < steps; i++) {
+    // +1 all octopuses
+    const steppedValues = recordedSteps[i].map((row) => row.map((value) => value + 1))
+
+    // Process flashed octopuses
+    steppedValues.forEach((row, rowIndex, allRows) => {
+      row.forEach((energy, energyIndex, allEnergy) => {
+        if (energy > 9) {
+          // Reset current item's energy to 0
+          row[energyIndex] = 0
+
+          const rowPrev = allRows[rowIndex - 1]
+          const rowNext = allRows[rowIndex + 1]
+
+          // Above
+          if (rowPrev) {
+            // Above-Left
+            if (typeof rowPrev[energyIndex - 1] !== 'undefined') {
+              rowPrev[energyIndex - 1] = rowPrev[energyIndex - 1] + 1
+            }
+            // Above-Center
+            rowPrev[energyIndex] = rowPrev[energyIndex] + 1
+            // Above-Right
+            if (typeof rowPrev[energyIndex + 1] !== 'undefined') {
+              rowPrev[energyIndex + 1] = rowPrev[energyIndex + 1] + 1
+            }
+          }
+
+          // Right
+          if (typeof allEnergy[energyIndex + 1] !== 'undefined') {
+            allEnergy[energyIndex + 1] = allEnergy[energyIndex + 1] + 1
+          }
+
+          // Below
+          if (rowNext) {
+            // Below-Left
+            if (typeof rowNext[energyIndex - 1] !== 'undefined') {
+              rowNext[energyIndex - 1] = rowNext[energyIndex - 1] + 1
+            }
+            // Below-Center
+            rowNext[energyIndex] = rowNext[energyIndex] + 1
+            // Below-Right
+            if (typeof rowNext[energyIndex + 1] !== 'undefined') {
+              rowNext[energyIndex + 1] = rowNext[energyIndex + 1] + 1
+            }
+          }
+
+          // Left
+          if (energyIndex > 0) {
+            allEnergy[energyIndex - 1] = allEnergy[energyIndex - 1] + 1
+          }
+        }
+      })
+    })
+
+    // Reset flashed octopuses (e.g. anything over 9)
+    // recordedSteps.push(steppedValues.map((row) => row.map((value) => (value > 9 ? 0 : value))))
+    recordedSteps.push(steppedValues)
+  }
+
+  return recordedSteps.map((state) => state.map((values) => values.join('')))
 }
 
 export const solvePart2 = async (input: string) => {
